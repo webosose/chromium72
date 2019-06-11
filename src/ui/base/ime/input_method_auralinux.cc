f00f4774f03398e5cdca3aea1ea99b067c5a708a
Eugene Cherkasov <eugene.cherkasov@lge.com>
2019-06-11 10:54:13 +0300

    [open][neva_upstreamable][ime] Dispatch both ET_KEY_PRESSED and ET_KEY_RELEASED post IME key events
    :Release Notes:
    Dispatch both ET_KEY_PRESSED and ET_KEY_RELEASED post IME key events
    
    :Detailed Notes:
    Javascript should be notified before and after input text changed.
    
    :Testing Performed:
    1. Navigate wam to https://mottie.github.io/Keyboard/
    2. Click MRCU pointer inside input field of QWERTY Text
    3. Enter some characters using VKB
    4. Now observe while entering characters using website specific VKB
    Verify that characters are appended at the end
    
    :QA Notes:
    Test-cases: GT-24713
    
    :Issues Addressed:
    [PLAT-67293] Chromium 2018Q4 upgrade: migrate 86b7f5365c [ime] Dispatch
                 both ET_KEY_PRESSED and ET_KEY_RELEASED post IME key events
    [PLAT-52458] Characters getting entered at the beginning instead of
                 getting appended
    [PLAT-52259] Unable to enter the character and vkb is not appearing.
    [PLAT-79991] Port patch: "[open][downstream][ime] Dispatch both
                 ET_KEY_PRESSED and ET_KEY_RELEASED post IME key events"
    
    Change-Id: I6295eaf9627dd4d18db3c27584255072ad3f60b0
    Originally-Reviewed-on: http://gpro.lge.com/218567
    Reviewed-on: http://gpro.lge.com/242890

--- src/ui/base/ime/input_method_auralinux.cc
+++ src/ui/base/ime/input_method_auralinux.cc
@@ -362,13 +362,17 @@ void InputMethodAuraLinux::OnCommit(const base::string16& text) {
   } else if (!IsTextInputTypeNone()) {
     // If we are not handling key event, do not bother sending text result if
     // the focused text input client does not support text input.
-    ui::KeyEvent event(ui::ET_KEY_PRESSED, ui::VKEY_PROCESSKEY, 0);
-    ui::EventDispatchDetails details = SendFakeProcessKeyEvent(&event);
+    ui::KeyEvent press_event(ui::ET_KEY_PRESSED, ui::VKEY_PROCESSKEY, 0);
+    ui::EventDispatchDetails details = SendFakeProcessKeyEvent(&press_event);
     if (details.dispatcher_destroyed)
       return;
-    if (!event.stopped_propagation() && !details.target_destroyed)
+    if (!press_event.stopped_propagation() && !details.target_destroyed)
       GetTextInputClient()->InsertText(text);
     composition_ = CompositionText();
+#if defined(OS_WEBOS)
+    ui::KeyEvent release_event(ui::ET_KEY_RELEASED, ui::VKEY_PROCESSKEY, 0);
+    SendFakeProcessKeyEvent(&release_event);
+#endif
   }
 }
 
@@ -389,12 +393,20 @@ void InputMethodAuraLinux::OnPreeditChanged(
     if (!composition_.text.empty() || !composition_text.text.empty())
       composition_changed_ = true;
   } else {
-    ui::KeyEvent event(ui::ET_KEY_PRESSED, ui::VKEY_PROCESSKEY, 0);
-    ui::EventDispatchDetails details = SendFakeProcessKeyEvent(&event);
-    if (details.dispatcher_destroyed)
-      return;
-    if (!event.stopped_propagation() && !details.target_destroyed)
-      GetTextInputClient()->SetCompositionText(composition_text);
+#if defined(OS_WEBOS)
+    if (!composition_.text.empty() || !composition_text.text.empty()) {
+#endif
+      ui::KeyEvent press_event(ui::ET_KEY_PRESSED, ui::VKEY_PROCESSKEY, 0);
+      ui::EventDispatchDetails details = SendFakeProcessKeyEvent(&press_event);
+      if (details.dispatcher_destroyed)
+        return;
+      if (!press_event.stopped_propagation() && !details.target_destroyed)
+        GetTextInputClient()->SetCompositionText(composition_text);
+#if defined(OS_WEBOS)
+      ui::KeyEvent release_event(ui::ET_KEY_RELEASED, ui::VKEY_PROCESSKEY, 0);
+      SendFakeProcessKeyEvent(&release_event);
+    }
+#endif
   }
 
   composition_ = composition_text;
@@ -412,12 +424,16 @@ void InputMethodAuraLinux::OnPreeditEnd() {
   } else {
     TextInputClient* client = GetTextInputClient();
     if (client && client->HasCompositionText()) {
-      ui::KeyEvent event(ui::ET_KEY_PRESSED, ui::VKEY_PROCESSKEY, 0);
-      ui::EventDispatchDetails details = SendFakeProcessKeyEvent(&event);
+      ui::KeyEvent press_event(ui::ET_KEY_PRESSED, ui::VKEY_PROCESSKEY, 0);
+      ui::EventDispatchDetails details = SendFakeProcessKeyEvent(&press_event);
       if (details.dispatcher_destroyed)
         return;
-      if (!event.stopped_propagation() && !details.target_destroyed)
+      if (!press_event.stopped_propagation() && !details.target_destroyed)
         client->ClearCompositionText();
+#if defined(OS_WEBOS)
+      ui::KeyEvent release_event(ui::ET_KEY_RELEASED, ui::VKEY_PROCESSKEY, 0);
+      SendFakeProcessKeyEvent(&release_event);
+#endif
     }
     composition_ = CompositionText();
   }
@@ -458,7 +474,7 @@ bool InputMethodAuraLinux::NeedInsertChar() const {
 
 ui::EventDispatchDetails InputMethodAuraLinux::SendFakeProcessKeyEvent(
     ui::KeyEvent* event) const {
-  KeyEvent key_event(ui::ET_KEY_PRESSED, ui::VKEY_PROCESSKEY, event->flags());
+  KeyEvent key_event(event->type(), ui::VKEY_PROCESSKEY, event->flags());
   ui::EventDispatchDetails details =
       DispatchKeyEventPostIME(&key_event, base::NullCallback());
   if (key_event.stopped_propagation())
