7bf003d14cc0af0df9d2652927c149e266f56fe0
Yuri Sorochkin <yuri.sorochkin@lge.com>
2019-11-19 12:18:23 +0300

    fixup! [ose][neva_upstreamable][ozone][wayland] Add support multi-display window and VKB
    :Release Notes:
    Add support for multi-display window and VKB
    
    :Detailed Notes:
    On the second display where the window is shown
    pointer enter is not received -> active text input window handle is not set.
    Keyboard enter is received for any window of a display.
    We should set an active input window handle by the handling of Keyboard enter also.
    
    :Testing Performed:
    Launch any app with VKB on the second display.
    
    for example as test steps from [PLAT-88898]
    luna-send -n 1 -f luna://com.webos.applicationManager/launch '{"id": "test.redirection",
        "params":{"target":"http://www.google.com", "displayAffinity": 1}}'
    
    VKB should launch automatically.
    
    :QA Notes:
    Test-cases: QWA-1316 QWA-1556
    
    :Issues Addressed:
    [PLAT-87480] Migrate multiple VKB patch to webOS OSE
    
    Change-Id: I0a4f4354769907834578841aadd30b542a037d19
    Originally-Reviewed-on: http://gpro.lge.com/263789
    Reviewed-on: http://gpro.lge.com/c/webosose/chromium72/+/263908
    Tested-by: Lokesh Kumar Goel <lokeshkumar.goel@lge.com>
    Reviewed-by: Lokesh Kumar Goel <lokeshkumar.goel@lge.com>

--- src/ozone/wayland/input/keyboard.cc
+++ src/ozone/wayland/input/keyboard.cc
@@ -111,6 +111,8 @@ void WaylandKeyboard::OnKeyboardEnter(void* data,
     static_cast<WaylandWindow*>(wl_surface_get_user_data(surface));
   if (window) {
     seat->SetEnteredWindowHandle(device_id, window->Handle());
+    seat->SetActiveInputWindow(window->GetDisplayId(),
+                               window->Handle());
     device->GetDispatcher()->KeyboardEnter(window->Handle());
   }
 }
